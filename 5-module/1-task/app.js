024281eb4b8c5bb32169b4c53d23b75f745ad43e
evgeniy <evgeniy.golubev@scrdairy.com>
2020-03-07 13:00:05 +0300

    add solution for module 5 task 1

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,17 +1,40 @@
-const path = require('path');
-const Koa = require('koa');
+const path = require("path");
+const Koa = require("koa");
 const app = new Koa();
 
-app.use(require('koa-static')(path.join(__dirname, 'public')));
-app.use(require('koa-bodyparser')());
+app.use(require("koa-static")(path.join(__dirname, "public")));
+app.use(require("koa-bodyparser")());
 
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+let subscribers = {};
+
+router.get("/subscribe", async (ctx, next) => {
+    const {r: id} = ctx.request.query;
+
+    ctx.req.on("close", () => {
+        delete subscribers[id];
+    });
+
+    const sPromise = new Promise((resolve, reject) => {
+        subscribers[id] = {ctx, resolve};
+    })
+
+    await sPromise;
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post("/publish", async (ctx, next) => {
+    const { message } = ctx.request.body;
+
+    for(id in subscribers) {
+        const sub = subscribers[id];
+        sub.ctx.body = message;
+        sub.resolve();
+    }
+    subscribers = {};
+
+    ctx.status = 200;
 });
 
 app.use(router.routes());
